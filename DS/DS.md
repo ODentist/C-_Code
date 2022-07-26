267d2f556dd42781d4d9a93269249d5d88663337
ODentist <o.dentistlsr@gmail.com>
2022-07-26 19:28:51 +0800

    2022/7/26 19.28

--- DS/DS.md
+++ DS/DS.md
@@ -201,3 +201,94 @@ int main()
 
 ```
 
+### 动态顺序表基本操作（C语言）
+
+```c
+#include <iostream>
+using namespace std;
+#include<stdlib.h>//malloc、free函数的头文件 
+#define InitSize 10 //默认的最大长度
+typedef struct{
+ int  *data;//指示动态分配数组的指针
+ int MaxSize; //顺序表的最大容量
+ int length;  //顺序表的当前长度 
+}SeqList; 
+//初始化
+void InitList(SeqList &L){
+ //用malloc 函数申请一片连续的存储空间
+ L.data =(int*)malloc(InitSize*sizeof(int)) ;
+ L.length=0;
+ L.MaxSize=InitSize;
+} 
+//增加动态数组的长度
+void IncreaseSize(SeqList &L,int len){
+ int *p=L.data;
+ L.data=(int*)malloc((L.MaxSize+len)*sizeof(int));
+ for(int i=0;i<L.length;i++){
+  L.data[i]=p[i];   //将数据复制到新区域 
+ }
+ L.MaxSize=L.MaxSize+len; //顺序表最大长度增加len
+ free(p);  //释放原来的内存空间 
+ 
+} 
+bool ListInsert(SeqList &L, int i, int e){ 
+    //判断i的范围是否有效
+    if(i<1||i>L.length+1) 
+        return false;
+    if(L.length>L.MaxSize) //当前存储空间已满，不能插入  
+        return false;
+
+    for(int j=L.length; j>i; j--){    //将第i个元素及其之后的元素后移
+        L.data[j]=L.data[j-1];
+    }
+    L.data[i-1]=e;  //在位置i处放入e
+    L.length++;      //长度加1
+    return true;
+}
+
+bool LisDelete(SeqList &L, int i, int &e){ // e用引用型参数 
+    //判断i的范围是否有效
+    if(i<1||i>L.length) 
+        return false;
+
+    e = L.data[i-1];    //将被删除的元素赋值给e
+
+    for(int j=L.length; j>i; j--){    //将第i个后的元素前移
+        L.data[j-1]=L.data[j];
+    }
+    L.length--;      //长度减1
+    return true;
+}
+double GetElem(SeqList L, int i){//ElemType GetElem(SqList L, int i)注意原本的数组数据类型
+    // ...判断i的值是否合法
+    //查找第i个值
+    return L.data[i-1];      //注意是i-1
+}
+//寻找数组内的元素
+int LocateElem(SeqList L, int e){
+    for(int i=0; i<L.length; i++)
+        if(L.data[i] == e)  
+            return i+1;     //数组下标为i的元素值等于e，返回其位序i+1
+    return 0;               //推出循环，说明查找失败
+}
+int main(void){
+ SeqList L; //声明一个顺序表
+ InitList(L);//初始化顺序表
+ IncreaseSize(L,5);
+ return 0; 
+}
+```
+
+#### 顺序存储的优缺点
+
+##### 优点
+
+随机访问特性，查找O(1)时间，存储密度高；
+逻辑上相邻的元素，物理上也相邻；
+无须为表中元素之间的逻辑关系而增加额外的存储空间；
+
+##### 缺点
+
+插入和删除需移动大量元素；
+当线性表长度变化较大时，难以确定存储空间的容量；
+造成存储空间的“碎片”
--- DS/list.cpp
+++ DS/list.cpp
@@ -19,94 +19,167 @@
 // 	}
 // 	return 0; 
 // }
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+
+// const int MaxSize = 100;
+// template <class DataType>
+// class SeqList
+// {
+// public:
+//     SeqList(){length=0;}            
+//     SeqList(DataType a[],int n);    
+//     ~SeqList(){}                    
+//     int Length(){return length;}    
+//     DataType Get(int i);            
+//     int Locate(DataType x);         
+//     void Insert(int i,DataType x);  
+//     DataType Delete(int i);         
+//     void PrintList();               
+// private:
+//     DataType data[MaxSize];         
+//     int length;                     
+// };
+
+// template <class DataType>
+// SeqList<DataType>::SeqList(DataType a[],int n)
+// {
+//     if(n>MaxSize) throw "wrong parameter";
+//     for(int i=0;i<n;i++)
+//         data[i]=a[i];
+//     length=n;
+// }
+
+// template <class DataType>
+// DataType SeqList<DataType>::Get(int i)
+// {
+//     if(i<1 && i>length) throw "wrong Location";
+//     else return data[i-1];
+// }
+
+// template <class DataType>
+// int SeqList<DataType>::Locate(DataType x)
+// {
+//     for(int i=0;i<length;i++)
+//         if(data[i]==x) return i+1;
+//     return 0;
+// }
+
+// template <class DataType>
+// void SeqList<DataType>::Insert(int i,DataType x)
+// {
+//     if(length>=MaxSize) throw "Overflow";
+//     if(i<1 || i>length+1) throw "Location";
+//     for(int j=length;j>=i;j--)
+//         data[j]=data[j-1];
+//     data[i-1]=x;
+//     length++;
+// }
+
+// template <class DataType>
+// DataType SeqList<DataType>::Delete(int i)
+// {
+//     int x;
+//     if(length==0) throw "Underflow";
+//     if(i<1 || i>length) throw "Location";
+//     x = data[i-1];
+//     for(int j=i;j<length;j++)
+//         data[j-1] = data[j];
+//     length--;
+//     return x;
+// }
+
+// template <class DataType>
+// void SeqList<DataType>::PrintList()
+// {
+//     for(int i=0;i<length;i++)
+//         cout<<data[i]<<endl;
+// }
+
+// int main()
+// {
+//     SeqList<int> p;
+//     p.Insert(1,5);
+//     p.Insert(2,9);
+//     p.PrintList();
+//     p.Insert(2,3);
+//     cout<<p.Length()<<endl;
+//     p.PrintList();
+//     cout<<p.Get(3)<<endl;
+//     p.Delete(2);
+//     p.PrintList();
+//     return 0;
+// }
+#include <iostream>
 using namespace std;
+#include<stdlib.h>//malloc、free函数的头文件 
+#define InitSize 10 //默认的最大长度
+typedef struct{
+	int  *data;//指示动态分配数组的指针
+	int MaxSize; //顺序表的最大容量
+	int length;  //顺序表的当前长度 
+}SeqList; 
+//初始化
+void InitList(SeqList &L){
+	//用malloc 函数申请一片连续的存储空间
+	L.data =(int*)malloc(InitSize*sizeof(int)) ;
+	L.length=0;
+	L.MaxSize=InitSize;
+} 
+//增加动态数组的长度
+void IncreaseSize(SeqList &L,int len){
+	int *p=L.data;
+	L.data=(int*)malloc((L.MaxSize+len)*sizeof(int));
+	for(int i=0;i<L.length;i++){
+		L.data[i]=p[i];   //将数据复制到新区域 
+	}
+	L.MaxSize=L.MaxSize+len; //顺序表最大长度增加len
+	free(p);  //释放原来的内存空间 
+	
+} 
+bool ListInsert(SeqList &L, int i, int e){ 
+    //判断i的范围是否有效
+    if(i<1||i>L.length+1) 
+        return false;
+    if(L.length>L.MaxSize) //当前存储空间已满，不能插入  
+        return false;
 
-const int MaxSize = 100;
-template <class DataType>
-class SeqList
-{
-public:
-    SeqList(){length=0;}            
-    SeqList(DataType a[],int n);    
-    ~SeqList(){}                    
-    int Length(){return length;}    
-    DataType Get(int i);            
-    int Locate(DataType x);         
-    void Insert(int i,DataType x);  
-    DataType Delete(int i);         
-    void PrintList();               
-private:
-    DataType data[MaxSize];         
-    int length;                     
-};
-
-template <class DataType>
-SeqList<DataType>::SeqList(DataType a[],int n)
-{
-    if(n>MaxSize) throw "wrong parameter";
-    for(int i=0;i<n;i++)
-        data[i]=a[i];
-    length=n;
+    for(int j=L.length; j>i; j--){    //将第i个元素及其之后的元素后移
+        L.data[j]=L.data[j-1];
+    }
+    L.data[i-1]=e;  //在位置i处放入e
+    L.length++;      //长度加1
+    return true;
 }
 
-template <class DataType>
-DataType SeqList<DataType>::Get(int i)
-{
-    if(i<1 && i>length) throw "wrong Location";
-    else return data[i-1];
-}
+bool LisDelete(SeqList &L, int i, int &e){ // e用引用型参数 
+    //判断i的范围是否有效
+    if(i<1||i>L.length) 
+        return false;
 
-template <class DataType>
-int SeqList<DataType>::Locate(DataType x)
-{
-    for(int i=0;i<length;i++)
-        if(data[i]==x) return i+1;
-    return 0;
-}
+    e = L.data[i-1];    //将被删除的元素赋值给e
 
-template <class DataType>
-void SeqList<DataType>::Insert(int i,DataType x)
-{
-    if(length>=MaxSize) throw "Overflow";
-    if(i<1 || i>length+1) throw "Location";
-    for(int j=length;j>=i;j--)
-        data[j]=data[j-1];
-    data[i-1]=x;
-    length++;
+    for(int j=L.length; j>i; j--){    //将第i个后的元素前移
+        L.data[j-1]=L.data[j];
+    }
+    L.length--;      //长度减1
+    return true;
 }
-
-template <class DataType>
-DataType SeqList<DataType>::Delete(int i)
-{
-    int x;
-    if(length==0) throw "Underflow";
-    if(i<1 || i>length) throw "Location";
-    x = data[i-1];
-    for(int j=i;j<length;j++)
-        data[j-1] = data[j];
-    length--;
-    return x;
+double GetElem(SeqList L, int i){//ElemType GetElem(SqList L, int i)注意原本的数组数据类型
+    // ...判断i的值是否合法
+    //查找第i个值
+    return L.data[i-1];      //注意是i-1
 }
-
-template <class DataType>
-void SeqList<DataType>::PrintList()
-{
-    for(int i=0;i<length;i++)
-        cout<<data[i]<<endl;
+//寻找数组内的元素
+int LocateElem(SeqList L, int e){
+    for(int i=0; i<L.length; i++)
+        if(L.data[i] == e)  
+            return i+1;     //数组下标为i的元素值等于e，返回其位序i+1
+    return 0;               //推出循环，说明查找失败
 }
-
-int main()
-{
-    SeqList<int> p;
-    p.Insert(1,5);
-    p.Insert(2,9);
-    p.PrintList();
-    p.Insert(2,3);
-    cout<<p.Length()<<endl;
-    p.PrintList();
-    cout<<p.Get(3)<<endl;
-    p.Delete(2);
-    p.PrintList();
-    return 0;
+int main(void){
+	SeqList L; //声明一个顺序表
+	InitList(L);//初始化顺序表
+	IncreaseSize(L,5);
+	return 0; 
 }
